1f448b6dc9de3837629e2c64530be84407e0e90a
Neelo16 <neelo_sim@hotmail.com>
2015-09-30 11:45:21 +0100

    Added array allocation on exit

--- src/parshell.c
+++ src/parshell.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
 #include "parshell.h"
 #include "commandlinereader.h"
 
 #define ARGNUM 7						// nome_comando + 5 argumentos(maximo) + argumento NULL
-#define CMDLENGTH 20
+
 int main(int argc, char const *argv[])
 {
 	char *argvector[ARGNUM];
 	int i;
-	int child_num = 0;
+	int childCnt = 0;
 
 	// Inicializar o vetor argvector
 	for(i=0;i<ARGNUM;i++)
 		argvector[i] = NULL;
+
+    while (1) {
+        int numArgs = readLineArguments(argvector, ARGNUM);
+        if (numArgs < 0)
+        {
+            perror("Error reading arguments");
+            exit(1);
+        }
+        if (!strcmp("exit", argvector[0])) {
+            int *pidArray = (int*) malloc(sizeof(int)*childCnt);
+            int *statusArray = (int*) malloc(sizeof(int)*childCnt);
+            for (i = 0; i < childCnt; i++) {
+                pidArray[i] = wait(statusArray + i); /* Address of the entry i of status array*/
+                
+            }
+        }
+    }
 
 	//exit
 	
