73ac343ef0399cbf549d2bd1a658f8265ea900b8
icemonster <nunossabino@hotmail.com>
2015-09-30 11:24:55 +0100

    Special changes to our main

--- src/parshell.c
+++ src/parshell.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parshell.h"
+#include "commandlinereader.h"
+
+#define ARGNUM 7						// nome_comando + 5 argumentos(maximo) + argumento NULL
+#define CMDLENGTH 20
 int main(int argc, char const *argv[])
 {
-	printf("Im awesome");
+	char *argvector[ARGNUM];
+	int i;
+	int child_num = 0;
+
+	// Inicializar o vetor argvector
+	for(i=0;i<ARGNUM;i++)
+		argvector[i] = NULL;
+
+	//exit
+	
 	return 0;
 }
