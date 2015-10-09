3100ef89f75cd6d11e0eeff0f9c3daf8b50b08bb
Neelo16 <neelo_sim@hotmail.com>
2015-10-09 22:01:44 +0100

    Changed condition on wait error check

--- src/parshell.c
+++ src/parshell.c
@@ -19,11 +19,9 @@ void exitShell(int childCnt) {
     else {
         for (i = 0; i < childCnt; i++) {
             pidArray[i] = wait(statusArray + i); /* Address of the entry i of status array*/
-            if (pidArray[i] == -1) {
+            if (pidArray[i] == -1)
                 perror("Error in wait");
-                continue;
-            }
-            if (!WIFEXITED(statusArray[i])) /* checks if child process terminated properly */
+            else if (!WIFEXITED(statusArray[i])) /* checks if child process terminated properly */
                 fprintf(stderr, "Error ocurred in child process\n");
         }
         for (i = 0; i < childCnt; i++)
