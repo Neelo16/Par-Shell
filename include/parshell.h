09714b1af91eccad2deb9eee421608d65462f219
Neelo16 <neelo_sim@hotmail.com>
2015-10-21 10:41:07 +0100

    Added struct to share data between threads

--- include/parshell.h
+++ include/parshell.h
@@ -1,7 +1,17 @@
 #ifndef __PARSHELL_H__
 #define __PARSHELL_H__
 
+#include <pthread.h>
+#include "list.h"
+
 #define ARGNUM 7
+
+typedef struct
+{
+   int childCnt;
+   pthread_mutex_t mutex;
+   list_t pidList;
+}* sharedData;
 
 void exitShell(int childCnt);
 int createProcess(char *argVector[]);
