7e89dd7c99fcd64b2bcf3603ad479882986dfcf9
Neelo16 <neelo_sim@hotmail.com>
2015-10-23 11:02:28 +0100

    Added perror in insert_new_process

--- src/list.c
+++ src/list.c
@@ -47,6 +47,8 @@ int insert_new_process(list_t *list, int pid, time_t starttime)
         list->first = item;
         return 1;
     }
+    else
+        perror("Error inserting process in list");
     return 0; /* If it failed to alocate memory to the new item */
 }
 
