796627b4816124734a1eacaf14f9212700712ba4
neelo <neelo_sim@hotmail.com>
2015-11-11 23:18:54 +0000

    Fixed critical bug in getTotalRuntime

--- src/util.c
+++ src/util.c
@@ -7,19 +7,23 @@ int getTotalRuntime(FILE *f) {
     int totalRuntime = 0;
     char c;
 
+    rewind(f);
+
     if (fgetc(f) == EOF)
       return 0;
 
     fseek(f, -4, SEEK_END); /* Sets the file position indicator to the last
                                digit in the total runtime number */
     while ((c = fgetc(f)) != ' ') {
-      
+        
       fseek(f, -2, SEEK_CUR); /* Undoes the position indicator increment
                                  cause by fgetc and seeks to the previous
                                  character */
     }
 
     fscanf(f, "%d", &totalRuntime);
+
+    fseek(f, 0, SEEK_END);
 
     return totalRuntime;
 }
