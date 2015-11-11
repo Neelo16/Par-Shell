89fc984d3c8437568d8bda0612bd5064b39e8959
Guilherme Santos <gui9santos@hotmail.com>
2015-11-11 11:26:17 +0000

    Changed parshell header to welcome the very dear condition variable

--- include/parshell.h
+++ include/parshell.h
@@ -36,11 +36,11 @@ void mutexLock(pthread_mutex_t *mutex);
 /* Unlocks the mutex and exits if there's an error */
 void mutexUnlock(pthread_mutex_t *mutex);
 
-/* Waits for the semaphore sem and exits on error */
-void semWait(sem_t *sem);
+/* Waits for the condition variable cond (unlocking its mutex) and exits on error */
+void condWait(pthread_cond_t *cond, pthread_mutex_t *mutex);
 
-/* Post the semaphore sem and exits on error */
-void semPost(sem_t *sem);
+/* Signal condition variable cond and exits on error */
+void condSignal(pthread_cond_t *cond);
 
 /* Reads given file and returns number of lines from current seek position */
 int getNumLines(FILE *f);
