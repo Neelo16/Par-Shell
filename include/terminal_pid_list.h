e9bc3e41e7b37ae22cb20df96b49e7e5e599cea5
neelo <neelo_sim@hotmail.com>
2015-12-02 09:27:38 +0000

    Completed the header for the TerminalList.

--- include/terminal_pid_list.h
+++ include/terminal_pid_list.h
@@ -1,9 +1,26 @@
 #ifndef __TERMINAL_PID_H__
 #define __TERMINAL_PID_H__
 
-typedef struct{
+typedef struct terminalListItem {
 	int pid;
-	terminalList_t *next;
-} terminalList_t;
+	terminalListItem *next;
+}* terminalList_t;
+
+/* Create a new list for storing pids. Returns NULL on error */
+terminalList_t createList();
+
+/* Insert a pid in the given list. Returns 0 on success, and a
+ * non-zero value on error.
+ */
+int insertPid(int pid, terminalList_t list);
+
+/** Remove the given pid from the list. */
+void removePid(int pid, terminalList_t list);
+
+/** Send a KILL signal to all pids in the list. */
+void killAllPids(terminalList_t list);
+
+/** Destroy the given list. */
+void destroyTerminalList(terminalList_t list);
 
 #endif
